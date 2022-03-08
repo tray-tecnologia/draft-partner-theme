f2d726932c352e6384586959bb4168e5dd7f78f8
Merieli Manzano <merieli.manzano@tray.net.br>
2022-03-08 18:03:04 -0300

    feat: definitions of 'footer', 'header ', 'menu' and 'banner'

--- theme/js/main.js
+++ theme/js/main.js
@@ -2,14 +2,37 @@
     window.theme = {
         ...window.theme,
 
+        settings: {},
+
+        // Close and Open and apply overlay
+        closeAndOpen: function () {
+            $('[data-toggle="closed"]').on('click', function () {
+                let target = $($(this).data('target'));
+                target.addClass('u-show').attr('data-toggle', 'open');
+
+                $('[data-overlay="shadow"]').addClass('u-show');
+                $('body').addClass('overflowed');
+            });
+
+            $('[data-overlay="shadow"]').on('click', function () {
+                $('[data-toggle="open"]').removeClass('u-show').removeAttr('data-toggle');
+                $('[data-overlay="shadow"]').removeClass('u-show');
+                $('body').removeClass('overflowed');
+            });
+
+            $('[data-toggle="close"]').on('click', function () {
+                $('[data-overlay="shadow"]').trigger('click');
+            });
+        },
+
         // Menu
         menuMobileDisplayCategories: function () {
-            $('.nav-mobile .first-level > .sub > a').on('click', function (event) {
+            $('.menuMobile-menu .menu-list > .subLists > .menu-link').on('click', function (event) {
                 let item = $(this).parent();
 
-                item.toggleClass('show');
+                item.toggleClass('u-show');
 
-                if (item.hasClass('show')) {
+                if (item.hasClass('u-show')) {
                     item.children('.sub').slideDown();
                 } else {
                     item.children('.sub').slideUp();
@@ -24,7 +47,8 @@
     // execução das funçoes
     $(() => {
         setTimeout(() => {
-            theme.menuMobileDisplay();
+            theme.closeAndOpen();
+            theme.menuMobileDisplayCategories();
         }, 20);
     });
 })(jQuery);
