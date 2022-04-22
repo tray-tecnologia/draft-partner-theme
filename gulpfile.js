a7189e466e775ab9d16e843294388b1f2c94d6a0
Merieli Manzano <merieli.manzano@tray.net.br>
2022-04-22 17:02:43 -0300

    build: changed function to watch modification of js and css files too, and inserted asynchronous execution of some tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,10 @@ const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const headerComment = require('gulp-header-comment');
-//const pkg = require('./package.json');
 const fs = require('fs');
 const args = require('yargs/yargs')(process.argv.slice(2)).argv;
 const { EOL } = require('os');
+const { parallel } = require('gulp');
 
 const paths = {
     theme: {
@@ -22,13 +22,17 @@ const paths = {
     },
 };
 
+const commentTemplate = `
+    @theme <%= pkg.theme %>
+    @version <%= pkg.version %>
+    @author <%= pkg.author %>
+    @site <%= pkg.homepage %>
+`;
+
 const minifiedTemplate = `{#
 # Attention!
 # This file was created automatically by compilation process.
 # Please do not modify this file if you do not know what you are doing.
-#
-# The files styles.tpl and scripts.tpl should exists in the path snipplets/layout
-# together with this file.
 #}`;
 
 /**
@@ -40,7 +44,7 @@ function createMinifiedVariableFile(cb) {
 
     fileData += args.local ? `${EOL}${EOL}{% set minified = false %}` : `${EOL}${EOL}{% set minified = true %}`;
 
-    fs.writeFileSync(`${paths.theme.elements}/minified.tpl`, fileData);
+    fs.writeFileSync(`${paths.theme.elements}/minified.html`, fileData);
 
     cb();
 }
@@ -71,15 +75,7 @@ function minifyCSS() {
         .src([`${paths.theme.css}/*.css`, `!${paths.theme.css}/*.min.css`], { sourcemaps: true })
         .pipe(postcss([cssnano]))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(
-            headerComment(
-                `@theme <%= pkg.theme %>
-             @version <%= pkg.version %>
-             @author <%= pkg.author %>
-             @site <%= pkg.homepage %>
-            `
-            )
-        )
+        .pipe(headerComment(commentTemplate))
         .pipe(gulp.dest(paths.theme.css));
 }
 
@@ -87,7 +83,10 @@ function minifyCSS() {
  * Watch any changes
  */
 function watchFiles(cb) {
-    gulp.watch(`${paths.theme.sass}/**/*.scss`, processSass, cb);
+    gulp.watch(`${paths.theme.sass}/**/*.scss`, () => gulp.series(processSass)());
+    gulp.watch(`${paths.theme.css}/**/*.css`, () => gulp.series(minifyCSS)());
+    gulp.watch(`${paths.theme.js}/**/*.js`, () => gulp.series(minifyJS)());
+    return cb();
 }
 
 /**
@@ -117,15 +116,7 @@ function minifyJS() {
         .src([`${paths.theme.js}/**/*.js`, `!${paths.theme.js}/**/*.min.js`], { sourcemaps: true })
         .pipe(terser({ mangle: { toplevel: true } }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(
-            headerComment(
-                `@theme <%= pkg.theme %>
-             @version <%= pkg.version %>
-             @author <%= pkg.author %>
-             @site <%= pkg.homepage %>
-            `
-            )
-        )
+        .pipe(headerComment(commentTemplate))
         .pipe(gulp.dest(paths.theme.js));
 }
 
@@ -133,8 +124,16 @@ function minifyJS() {
  * GULP TASKS
  * Tarefas que poderão ser executadas pelo gulp.
  */
-const build = gulp.series(createMinifiedVariableFile, processSass, minifyCSS, concatLibsJs, minifyJS);
-const start = gulp.series(createMinifiedVariableFile, processSass, concatLibsJs, minifyJS, watchFiles);
+const build = gulp.series(
+    parallel(createMinifiedVariableFile, processSass, concatLibsJs),
+    parallel(minifyCSS, minifyJS)
+);
+
+const start = gulp.series(
+    parallel(createMinifiedVariableFile, processSass, concatLibsJs),
+    parallel(minifyCSS, minifyJS),
+    watchFiles
+);
 
 exports.generate = createMinifiedVariableFile;
 exports.build = build;
